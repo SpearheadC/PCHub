b5d222dc51c093e33f0b56257c5d367a227bcd56
SpearheadC <valentina@spearhead.construction>
2026-09-08 09:03:42 -0300

    Delete data directory

--- data/groups-merge.js
+++
@@ -1,2 +0,0 @@
-/* Junta los pedazos de GROUPS en un solo objeto. Si agregás un data/groups-*.js nuevo, sumalo acá. */
-const GROUPS = Object.assign({}, GROUPS_TEMPLATES, GROUPS_CONSTRUCTION, GROUPS_SDGE);
